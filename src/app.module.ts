4c896b76632776a2de92dd4b7a84ffbdad80aa46
Amirtha <amirtha4501@gmail.com>
2023-09-28 20:30:19 +0530

    Initial commit: NestJS, TypeORM boilerplate setup

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { configService } from './config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
